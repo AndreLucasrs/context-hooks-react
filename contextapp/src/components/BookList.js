c7a8a6116c7463375b29d94455e0db4443c3116a
André Lucas <andrelrs.v@outlook.com>
2020-02-20 22:36:39 -0300

    changed class component to function component

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React, { useContext} from "react";
 import {ThemeContext} from "../context/ThemeContext";
 
-class BookList extends Component {
+const BookList = () => {
 
-    static contextType = ThemeContext;
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
 
-    render() {
+    const theme = isLightTheme ? light : dark;
 
-        const { isLightTheme, light, dark } = this.context;
+    return (
 
-        const theme = isLightTheme ? light : dark;
-
-        return (
-            <div className="book-list" style={{background: theme.bg, color: theme.syntax }} >
-                <ul>
-                    <li style={{background: theme.ui}}>the way of kings</li>
-                    <li style={{background: theme.ui}}>the name of the wind</li>
-                    <li style={{background: theme.ui}}>the final empire</li>
-                </ul>
-            </div>
-        );
-    }
+        <div className="book-list" style={{background: theme.bg, color: theme.syntax }} >
+            <ul>
+                <li style={{background: theme.ui}}>the way of kings</li>
+                <li style={{background: theme.ui}}>the name of the wind</li>
+                <li style={{background: theme.ui}}>the final empire</li>
+            </ul>
+        </div>
+    );
 }
 
 export default BookList;
